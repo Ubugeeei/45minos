2e99e6c112ab7610d477533bd6b6f0963e971dcc
Ubugeeei <ubuge1122@gmail.com>
2023-09-03 12:18:57 +0900

    tmp: context switch

--- 060-context-switch/source/src/kernel.rs
+++ 060-context-switch/source/src/kernel.rs
@@ -15,27 +15,32 @@ struct SBIRet {
     value: usize,
 }
 
-fn sbi_call(eid: usize, fid: usize, arg0: usize, arg1: usize, arg2: usize) -> SBIRet {
+#[allow(clippy::too_many_arguments)]
+#[inline(always)]
+fn sbi_call(
+    ext_id: usize,
+    fn_id: usize,
+    arg0: usize,
+    arg1: usize,
+    arg2: usize,
+    arg3: usize,
+    arg4: usize,
+    arg5: usize,
+) -> SBIRet {
     let error: usize;
     let value: usize;
 
     unsafe {
         asm!(
-            "mv a0, {arg0}",
-            "mv a1, {arg1}",
-            "mv a2, {arg2}",
-            "mv a7, {eid}",
-            "mv a6, {fid}",
             "ecall",
-
-            arg0 = in(reg) arg0,
-            arg1 = in(reg) arg1,
-            arg2 = in(reg) arg2,
-            eid = in(reg) eid,
-            fid = in(reg) fid,
-
-            out("a0") error,
-            out("a1") value,
+            inout("a0") arg0 => value,
+            in("a1") arg1,
+            in("a2") arg2,
+            in("a3") arg3,
+            in("a4") arg4,
+            in("a5") arg5,
+            in("a6") fn_id,
+            inout("a7") ext_id => error
         );
     }
 
@@ -43,7 +48,7 @@ fn sbi_call(eid: usize, fid: usize, arg0: usize, arg1: usize, arg2: usize) -> SB
 }
 
 fn put_char(ch: u8) {
-    sbi_call(0x01, 1, ch as usize, 0, 0);
+    sbi_call(0x01, 0x00, ch as usize, 0, 0, 0, 0, 0);
 }
 
 const BUFFER_SIZE: usize = 128;
@@ -112,6 +117,105 @@ extern "C" fn trap_handler() {
     loop {}
 }
 
+#[inline(always)]
+unsafe extern "C" fn switch_context(old_sp: usize, new_sp: usize) {
+    asm!(
+        "addi sp, sp, -13 * 4",
+        // store
+        "sw ra, 0 * 4(sp)",
+        "sw s0, 1 * 4(sp)",
+        "sw s1, 2 * 4(sp)",
+        "sw s2, 3 * 4(sp)",
+        "sw s3, 4 * 4(sp)",
+        "sw s4, 5 * 4(sp)",
+        "sw s5, 6 * 4(sp)",
+        "sw s6, 7 * 4(sp)",
+        "sw s7, 8 * 4(sp)",
+        "sw s8, 9 * 4(sp)",
+        "sw s9, 10 * 4(sp)",
+        "sw s10, 11 * 4(sp)",
+        "sw s11, 12 * 4(sp)",
+        "sw sp, ({old_sp})",
+        // load
+        "lw sp, ({new_sp})",
+        "lw ra, 0 * 4(sp)",
+        "lw s0, 1 * 4(sp)",
+        "lw s1, 2 * 4(sp)",
+        "lw s2, 3 * 4(sp)",
+        "lw s3, 4 * 4(sp)",
+        "lw s4, 5 * 4(sp)",
+        "lw s5, 6 * 4(sp)",
+        "lw s6, 7 * 4(sp)",
+        "lw s7, 8 * 4(sp)",
+        "lw s8, 9 * 4(sp)",
+        "lw s9, 10 * 4(sp)",
+        "lw s10, 11 * 4(sp)",
+        "lw s11, 12 * 4(sp)",
+        "addi sp, sp, 13 * 4",
+        "ret",
+        old_sp = in(reg) old_sp,
+        new_sp = in(reg) new_sp,
+    );
+}
+
+const STACK_SIZE: usize = 8192;
+
+#[repr(C)]
+struct Thread {
+    sp: usize,
+    stack: [usize; STACK_SIZE],
+}
+
+impl Thread {
+    fn init(&mut self, entry: extern "C" fn(*mut ())) {
+        let sp = self.stack.as_mut_ptr() as *mut usize;
+        let mut sp = unsafe { sp.add(STACK_SIZE / core::mem::size_of::<usize>()) };
+
+        for i in 0..12 {
+            unsafe {
+                sp = sp.offset(-1);
+                *sp = 0;
+                println!("sp: {:#x} (s{})", sp as usize, 11 - i);
+            }
+        }
+
+        unsafe {
+            sp = sp.offset(-1);
+            *sp = entry as usize;
+            println!("sp: {:#x} (ra)", sp as usize);
+        } // ra
+        self.sp = sp as usize;
+        println!("sp: {:#x} (sp)", self.sp);
+    }
+}
+
+static mut THREAD_A: Thread = Thread {
+    sp: 0,
+    stack: [0; STACK_SIZE],
+};
+static mut THREAD_B: Thread = Thread {
+    sp: 0,
+    stack: [0; STACK_SIZE],
+};
+
+extern "C" fn thread_a_entry(_arg: *mut ()) {
+    loop {
+        unsafe {
+            println!("A");
+            switch_context(THREAD_A.sp, THREAD_B.sp);
+        }
+    }
+}
+
+extern "C" fn thread_b_entry(_arg: *mut ()) {
+    loop {
+        unsafe {
+            println!("B");
+            switch_context(THREAD_B.sp, THREAD_A.sp);
+        }
+    }
+}
+
 extern "C" {
     static __stack_top: u8;
 }
@@ -132,15 +236,17 @@ pub unsafe extern "C" fn boot() -> ! {
 
 #[allow(dead_code)]
 fn kernel_main() {
-    unsafe {
-        asm!("csrw stvec, {0}", in(reg) trap_handler);
-    }
+    unsafe { asm!("csrw stvec, {0}", in(reg) trap_handler) }
 
-    println!("Hello, world! {:#04x}", 1);
+    unsafe { THREAD_A.init(thread_a_entry) };
+    unsafe { println!("THREAD_A.sp: {:#x}", THREAD_A.sp) }
+    unsafe { THREAD_B.init(thread_b_entry) };
+    unsafe { println!("THREAD_B.sp: {:#x}", THREAD_B.sp) }
+    let tmp = &0 as *const _ as usize;
+    println!("tmp: {:#x}", tmp);
+    unsafe { switch_context(tmp, THREAD_A.sp) };
 
-    unsafe {
-        asm!("ebreak");
-    }
+    println!("unreachable");
 
     #[allow(clippy::empty_loop)]
     loop {}
